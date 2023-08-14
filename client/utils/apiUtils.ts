7f29c9365795198a88ce70d32fc68ee03a68d81d
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-08-13 23:10:39 -0700

    added guest List api and frontend

--- client/utils/apiUtils.ts
+++ client/utils/apiUtils.ts
@@ -15,3 +15,8 @@ export const rsvp = async (email:string, name:string, attending:boolean, guest?:
     console.log(response.data);
     return response.data;
 }
+
+export const getGuestList = async (username: string, password: string) => {
+    const response = await axios.get('http://localhost:5000/api/guestList?username=' + username + '&password=' + password);
+    return response;
+}
--- server/app.py
+++ server/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 from api import db
 from flask_cors import CORS, cross_origin
+import bcrypt
 
 app = Flask(__name__)
+
 @app.route('/api/rsvp', methods=['POST'])
 @cross_origin()
-def signup():
+def rsvp():
     invitees_collection = db['Invitees']
     rsvp_collection = db['rsvp']
     user_data = request.json
@@ -17,12 +19,48 @@ def signup():
         return jsonify({'message': 'This email address has already responded', 'status': 400})
     # Insert user data into the database
     user_id = rsvp_collection.insert_one({
+        'name': user_data['name'],
         'email': user_data['email'],
         'attending': user_data['attending'],
         'guest': user_data['guest'],
     }).inserted_id
 
     return jsonify({'message': 'User registered successfully', 'user_id': str(user_id)})
+
+@app.route('/api/guestList', methods=['GET'])
+@cross_origin()
+def guestList():
+    admin_collection = db['admins']
+    rsvp_collection = db['rsvp']
+    password = request.args.get('password')
+    user = admin_collection.find_one({'username': request.args.get('username')})
+    if user and bcrypt.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
+        guests = []
+        for guest in rsvp_collection.find():
+            guests.append({
+                'name': guest['name'],
+                'email': guest['email'],
+                'attending': guest['attending'],
+                'guest': guest['guest'],
+            })
+        return jsonify(guests)
+    else:
+        return jsonify({'message': 'Invalid username and password', 'status': 400})
+
+# @app.route('/api/signup', methods=['POST'])
+# @cross_origin()
+# def signup():
+#     users_collection = db['admins']
+#     password = ''
+#     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+#     user_data = {
+#         'username': '',
+#         'password': hashed_password.decode('utf-8')
+#         # Other user data
+#     }
+
+#     users_collection.insert_one(user_data)
 
 if __name__ == '__main__':
     app.run()
