bca4ac265f53a68956576b452df3c4f462189bf1
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-09-09 11:36:09 -0700

    Removed margin, padding, added page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,22 +1,53 @@
 import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+  Typography,
+} from "@mui/material";
 import RsvpModal from "@/components/modals/rsvpModal";
 import style from "@/styles/Home.module.css";
 
 export default function Home() {
   const [showRsvpModal, setShowRsvpModal] = useState(false);
 
+  const scrollToBottom = () => {
+    const targetPosition = window.innerHeight;
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className={style["background-container"]}>
-      <Button
-        className={style["rsvp-button"]}
-        onClick={() => setShowRsvpModal(true)}
-        size="large"
-        variant="contained"
-      >
-        RSVP
-      </Button>
-      {showRsvpModal && <RsvpModal onClose={() => setShowRsvpModal(false)} />}
-    </div>
+    <Stack direction="column" className={style["container"]}>
+      <div className={style["main-page"]}>
+        <Button
+          className={style["rsvp-button"]}
+          onClick={() => setShowRsvpModal(true)}
+          size="large"
+          variant="contained"
+        >
+          RSVP
+        </Button>
+        {showRsvpModal && <RsvpModal onClose={() => setShowRsvpModal(false)} />}
+        <Button onClick={scrollToBottom}>important info</Button>
+      </div>
+      <Box className={style["important-info"]}>
+        <Typography className={style["cursive"]} variant="h1">
+          here are some important things!
+        </Typography>
+        <Typography className={style["cursive"]} variant="h3">
+          Accomodation Details:
+        </Typography>
+        <Card>
+          <CardContent>Asfsdfsd</CardContent>
+        </Card>
+      </Box>
+    </Stack>
   );
 }
