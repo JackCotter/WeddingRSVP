84f6a1c6745af915d2891871563a90f0ee6d9bff
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-09-17 16:13:23 -0700

    Cheeky main push, font import bug

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
+import { Dancing_Script } from "next/font/google";
 import type { AppProps } from "next/app";
 import theme from "@/utils/theme";
 import { ThemeProvider } from "@emotion/react";
 
+const dancing_script = Dancing_Script({
+  variable: "--font-dancing-script",
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <Component {...pageProps} className={dancing_script.variable} />
     </ThemeProvider>
   );
 }
