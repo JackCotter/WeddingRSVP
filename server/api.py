15b7e5dece49f22344b48961a5f03b6a30db8073
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-09-17 18:19:03 -0700

    Deleted api.py, moved from config json to .env

--- server/api.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import json
-
-config = {}
-with open('config.json') as f:
-    config = json.load(f)
-
-client = MongoClient(f"mongodb+srv://{config['username']}:{config['password']}@{config['cluster']}.6ba2vaf.mongodb.net/?retryWrites=true&w=majority")
-db = client['wedding']
