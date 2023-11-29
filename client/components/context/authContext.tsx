8343982792ddca4d0ce46e7c17142dfdbb4d2543
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-11-28 21:40:27 -0800

    auth in local storage, persists on refresh

--- client/components/context/authContext.tsx
+++ client/components/context/authContext.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 interface authContextType {
   authString: string | undefined;
@@ -22,10 +28,24 @@ interface authProviderProps {
 export const AuthContextProvider = ({ children }: authProviderProps) => {
   const [authString, setAuthString] = useState<string | undefined>(undefined);
 
+  const storeAuth = (authString: string | undefined) => {
+    setAuthString(authString);
+    if (typeof window !== "undefined" && authString !== undefined) {
+      localStorage.setItem("authstr", authString as string);
+    }
+  };
+
   const authContextValue = {
     authString,
-    setAuthString,
+    setAuthString: storeAuth,
   };
+
+  useEffect(() => {
+    const authstr = localStorage.getItem("authstr");
+    if (authstr !== null) {
+      setAuthString(authstr);
+    }
+  }, []);
 
   return (
     <authContext.Provider value={authContextValue}>
