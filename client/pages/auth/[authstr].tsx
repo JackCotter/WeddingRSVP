7b92052e6367f45821dad122d012218c0e11e426
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-11-28 23:43:48 -0800

    added loadin page

--- client/pages/auth/[authstr].tsx
+++ client/pages/auth/[authstr].tsx
@@ -1,4 +1,5 @@
 import { useAuth } from "@/components/context/authContext";
+import { CircularProgress, Stack, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect, useMemo } from "react";
 
@@ -13,7 +14,34 @@ const AuthPage = () => {
     router.push("/");
   }, [authstr]);
 
-  return <div></div>;
+  return (
+    <Stack
+      direction="row"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Stack
+        direction="column"
+        spacing={2}
+        style={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <CircularProgress
+          style={{
+            display: "flex",
+          }}
+        />
+        <Typography> Authenticating... </Typography>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default AuthPage;
