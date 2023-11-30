658013d7e9a748064a6291f7fbbaeb190ed62761
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-11-30 11:21:06 -0800

    loop to create n invites

--- invite-generator/invite_generator.py
+++ invite-generator/invite_generator.py
@@ -1,20 +1,35 @@
 import qrcode
 from bs4 import BeautifulSoup
 from PIL import Image
+import sys
+import string
+import secrets
 
 
-def main():
-    img = qrcode.make(
-        'https://wedding-rsvp-5506heh7o-jackcotter.vercel.app/auth/')
-    type(img)  # qrcode.image.pil.PilImage
-    # img.save("some_file.png")
-    background = Image.open("base.png")
-    img.thumbnail((180, 180))
-    img = img.convert("RGBA")
+def generate_token():
+    alphabet = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(alphabet) for _ in range(30))
 
-    background.paste(img, (300, 560), img)
-    background.show()
+
+def generate_invites(number_invites):
+    for invite_number in range(number_invites):
+        token = str(invite_number).zfill(3) + generate_token()
+        qr_url = 'https://wedding-rsvp-5506heh7o-jackcotter.vercel.app/auth/' + token
+        img = qrcode.make(qr_url)
+
+        background = Image.open("base.png")
+        img.thumbnail((180, 180))
+        img = img.convert("RGBA")
+
+        background.paste(img, (300, 560), img)
+        background.save("./completed_invites/invite" +
+                        str(invite_number).zfill(3) + ".png", "PNG")
+
+
+def main(number_invites):
+    generate_invites(int(number_invites))
 
 
 if __name__ == "__main__":
-    main()
+    number_invites = sys.argv[1]
+    main(number_invites)
